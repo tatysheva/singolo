973508e9d7e4399a18a8bc0102291ee23d0a741c
Aidan Tatysheva <tatyshevaaidan@gmail.com>
2020-04-01 20:00:13 +0600

    feat: add slider

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// NAVIGATION
+
 document.addEventListener('scroll', onScroll);
 
 function onScroll(event) {
@@ -17,3 +19,55 @@ function onScroll(event) {
         }
     })
 }
+
+// SLIDER
+
+let items = document.querySelectorAll('.carousel-item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+    currentItem = (items.length + n) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('carousel-active', direction);
+    });
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('carousel-next', direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('carousel-next', direction);
+        this.classList.add('carousel-active');
+        isEnabled = true;
+    });
+}
+
+
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n-1);
+    showItem('from-left');
+}
+
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n+1);
+    showItem('from-right');
+}
+
+document.querySelector(".arrow-left").addEventListener('click', function() {
+    if (isEnabled) {
+        previousItem(currentItem);
+    }
+})
+
+document.querySelector(".arrow-right").addEventListener('click', function() {
+    if (isEnabled) {
+        nextItem(currentItem);
+    }
+})
